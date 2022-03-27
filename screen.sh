cc6396d0fda2f55547cdf63c9fdc3445d44ff13c
Ubuntu <satim@satimdev.nf1zzrwcltuehpasykpggmt3vd.tx.internal.cloudapp.net>
2022-03-27 11:10:48 +0000

    modifying screen setup

--- screen.sh
+++ screen.sh
@@ -1,1 +1,2 @@
-sudo mv .screenrc ~/.screenrc
+echo -e "startup_message off\nvbell off\nautodetach on\nhardstatus alwayslastline\nhardstatus string '%{wk}%?%-Lw%?%{=b kR}(%{W}%n*%f %t%?(%u)%?%{=b kR})%{= w}%?%+Lw%?%? %{g}]'" > ~/.screenrc
+
