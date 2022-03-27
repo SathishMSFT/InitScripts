3ad639b11a6905933a86b575c9df1fb9d9d3144e
Ubuntu <satim@satimdev.nf1zzrwcltuehpasykpggmt3vd.tx.internal.cloudapp.net>
2022-03-27 12:42:09 +0000

    wrapper script changes

--- runSetup.sh
+++ runSetup.sh
@@ -1,3 +1,13 @@
+https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/go.sh 
+https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/docker.sh 
+https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/kubectl.sh 
+https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/helmSpecific.sh 
+https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/kubebuilder.sh 
+https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/kustomize.sh 
+https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/screen.sh 
+https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/ohmyposh.sh 
+
+
 sh ./go.sh 
 sh ./docker.sh 
 sh ./kubectl.sh 
