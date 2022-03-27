b4c8deedb596d925c219b6767b67252804f0f072
Ubuntu <satim@satimdev.nf1zzrwcltuehpasykpggmt3vd.tx.internal.cloudapp.net>
2022-03-27 12:53:57 +0000

    fix issues

--- runSetup.sh
+++ runSetup.sh
@@ -1,11 +1,11 @@
-https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/go.sh 
-https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/docker.sh 
-https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/kubectl.sh 
-https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/helmSpecific.sh 
-https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/kubebuilder.sh 
-https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/kustomize.sh 
-https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/screen.sh 
-https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/ohmyposh.sh 
+wget https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/go.sh 
+wget https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/docker.sh 
+wget https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/kubectl.sh 
+wget https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/helmSpecific.sh 
+wget https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/kubebuilder.sh 
+wget https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/kustomize.sh 
+wget https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/screen.sh 
+wget https://raw.githubusercontent.com/SathishMSFT/InitScripts/main/ohmyposh.sh 
 
 
 sh ./go.sh 
